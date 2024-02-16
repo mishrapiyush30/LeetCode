dd535cefac32c2be6c96006377f09c9ee4008af6
mishrapiyush30 <114108234+mishrapiyush30@users.noreply.github.com>
2024-02-16 18:19:09 -0500

    Time: 24 ms (92.34%), Space: 13.5 MB (63.91%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        char_map = [0] * 26
+
+        for i in range(len(s)):
+            char_map[ord(s[i]) - ord('a')] += 1
+            char_map[ord(t[i]) - ord('a')] -= 1
+
+        return all(count == 0 for count in char_map)
