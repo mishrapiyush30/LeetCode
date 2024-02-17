8050e27a0fa5555da3cf029454f56c14bfca32b7
mishrapiyush30 <114108234+mishrapiyush30@users.noreply.github.com>
2024-02-16 20:12:11 -0500

    Time: 69 ms (71.66%), Space: 18.1 MB (20.54%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        res = defaultdict(list)  # mapping charCount to list of Anagrams
+
+        for s in strs:
+            count = [0] * 26  # a ... z
+
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            res[tuple(count)].append(s)
+
+        return res.values()
